86bbb9a502fcb9c6dcaafc27970196b6cf33d894
Craig Olson <craigdolson@gmail.com>
2023-08-29 10:54:35 -0500

    adding python back to vscode

--- modules/hm/vscode.nix
+++ modules/hm/vscode.nix
@@ -6,7 +6,7 @@
      programs.vscode.enableExtensionUpdateCheck = true;
      programs.vscode.enableUpdateCheck = true;
      programs.vscode.extensions = [
-	      #pkgs.vscode-extensions.ms-python.python
+	pkgs.vscode-extensions.ms-python.python
         pkgs.vscode-extensions.jnoortheen.nix-ide
         pkgs.vscode-extensions.dracula-theme.theme-dracula
          ];
