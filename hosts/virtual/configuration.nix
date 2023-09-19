6578f5dacf85e695b86d0cae23d34aaf4b7cdfae
craig <craigdolson@gmail.com>
2023-09-19 07:59:18 -0500

    updating flake

--- hosts/virtual/configuration.nix
+++ hosts/virtual/configuration.nix
@@ -66,7 +66,7 @@ in
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.windowManager.bspwm.enable = true;
-  services.xserver.windowManager.qtile.enable = true;
+  #services.xserver.windowManager.qtile.enable = true;
 
   # MISC Services to enable
   services.pcscd.enable = true;
