036a65a7cc8c79c7408598abbcc9f45365cbdcc6
Craig Olson <craigdolson@gmail.com>
2023-10-03 10:11:31 -0500

    modify workspace colors qtile

--- hosts/opx-5090/home-manager/dotfiles/qtile/config.py
+++ hosts/opx-5090/home-manager/dotfiles/qtile/config.py
@@ -260,9 +260,9 @@ screens = [
                            padding = 8,
                            ), 
                  widget.GroupBox(
-                                highlight_color = colors[8],
-                                highlight_method = "line",
-                                other_screen_border = colors[6],
+                                highlight_color = colors[9],
+                                highlight_method = "block",
+                                ##other_screen_border = colors[6],
                                 active = colors[12],
                                 inactive = colors[1],
                                 margin_y = 4,
