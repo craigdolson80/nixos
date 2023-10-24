99e0297e4e9115d941d7464259a581e23f48307c
Craig Olson <craigdolson@gmail.com>
2023-10-24 07:50:03 -0500

    changing qtile groupbox color

--- hosts/opx-5090/home-manager/dotfiles/qtile/config.py
+++ hosts/opx-5090/home-manager/dotfiles/qtile/config.py
@@ -260,7 +260,7 @@ screens = [
                            padding = 8,
                            ), 
                  widget.GroupBox(
-                                highlight_color = colors[9],
+                                highlight_color = colors[6],
                                 highlight_method = "line",
                                 ##other_screen_border = colors[6],
                                 ##block_highlight_text_color = colors[9],
