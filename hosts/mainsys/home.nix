ac7f09148b5b674836affcd6f6b715c437d866fa
Craig Olson <craigdolson@gmail.com>
2023-11-21 17:14:19 -0600

    adding hyprland

--- hosts/mainsys/home.nix
+++ hosts/mainsys/home.nix
@@ -54,4 +54,17 @@
      recursive = true;
      executable = true;
      };
+  #hyprland
+     home.file.".config/hypr" = {
+     source = home-manager/dotfiles/hypr;
+     recursive = true;
+     executable = true;
+     };
+  #waybar
+     home.file.".config/waybar" = {
+     source = home-manager/dotfiles/waybar;
+     recursive = true;
+     executable = true;
+     };   
+    
 }
