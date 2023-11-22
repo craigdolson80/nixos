16484f9eb5187981da5019c9e6a88773a14a4d16
Craig Olson <craigdolson@gmail.com>
2023-11-22 13:54:47 -0600

    adding swaylock-effects

--- hosts/mainsys/home-manager/dotfiles/swayidle/lockscreen.sh
+++ hosts/mainsys/home-manager/dotfiles/swayidle/lockscreen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 timeswaylock=300
-timeoff=420
+timeoff=360
 swayidle -w timeout $timeswaylock 'swaylock -f -i ~/github/wallpaper/096.jpg' timeout $timeoff 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
--- hosts/mainsys/home-manager/dotfiles/swayidle/lockscreen_fast.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-timeoff=30
-swaylock -f -i '~/github/wallpaper/096.jpg' timeout $timeoff 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
