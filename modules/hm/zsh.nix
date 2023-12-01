dfbeb9df3a714130adf0b28f509473113f60ae8c
Craig Olson <craigdolson@gmail.com>
2023-11-30 21:06:36 -0600

    changing zsh theme

--- modules/hm/zsh.nix
+++ modules/hm/zsh.nix
@@ -8,7 +8,7 @@
     oh-my-zsh = {
       enable = true;
       plugins = [ "ssh-agent" "git" ];
-      theme = "fishy";
+      theme = "jaischeema";
     };
     initExtra = ''
       bindkey '^f' autosuggest-accept
