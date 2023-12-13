6ee970f4c1b76a2a45cf7e6d56c8cf8ade6478c1
Craig Olson <craigdolson@gmail.com>
2023-12-13 13:08:41 -0600

    moving external dotfiles to standalone .nix file

--- modules/hm/dotfiles.nix
+++ modules/hm/dotfiles.nix
@@ -10,8 +10,60 @@ in
 #DOTFILE IMPORTS
   #qtile
      home.file.".config/qtile" = {
-     source = /home/"${user}"/nixos/hosts/opx5090/home-manager/dotfiles/qtile;
+     source = /home/${user}/nixos/hosts/opx-5090/home-manager/dotfiles/qtile;
      recursive = true;
      };
+
+     home.file.".config/polybar" = {
+     source = /home/${user}/nixos/hosts/opx-5090/home-manager/dotfiles/polybar;
+     recursive = true;
+     executable = true;
+     };
+   #picom
+     home.file.".config/picom" = {
+     source = .commonfiles/picom;
+     };
+  #rofi
+     home.file.".config/rofi" = {
+     source = .commonfiles/rofi;
+     recursive = true;
+     executable = true;
+     };
+  #hyprland
+     home.file.".config/hypr" = {
+     source = /home/${user}/nixos/hosts/opx-5090/home-manager/dotfiles/hypr;
+     recursive = true;
+     executable = true;
+     };
+  #waybar
+     home.file.".config/waybar" = {
+     source = /home/${user}/nixos/hosts/opx-5090/home-manager/dotfiles/waybar;
+     recursive = true;
+     executable = true;
+     };   
+  #swaylock
+     home.file.".swaylock/" = {
+     source = /home/${user}/nixos/hosts/opx-5090/home-manager/dotfiles/swaylock;
+     recursive = true;
+     executable = true;
+     }; 
+   #swayidle
+     home.file."swayidle/" = {
+     source = /home/${user}/nixos/hosts/opx-5090/home-manager/dotfiles/swayidle;
+     recursive = true;
+     executable = true;
+     };  
+  #wlogout
+     home.file.".config/wlogout" = {
+     source = /home/${user}/nixos/hosts/opx-5090/home-manager/dotfiles/wlogout;
+     recursive = true;
+     executable = true;
+     };
+  #neofetch
+     home.file.".config/neofetch" = {
+     source = /home/${user}/nixos/hosts/opx-5090/home-manager/dotfiles/neofetch;
+     recursive = true;
+     executable = true;
+     };    
 
 }
