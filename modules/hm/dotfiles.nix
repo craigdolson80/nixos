29b80c49b6de7e09849f7fac662a0dd74ca33de4
Craig Olson <craigdolson@gmail.com>
2023-12-13 13:01:37 -0600

    moving external dotfiles to standalone .nix file

--- modules/hm/dotfiles.nix
+++ modules/hm/dotfiles.nix
@@ -1,12 +1,16 @@
 {config, pkgs, ...}:
 
+let
+  user="craig";
+in
+
 {
    
 
 #DOTFILE IMPORTS
   #qtile
      home.file.".config/qtile" = {
-     source = home-manager/dotfiles/qtile;
+     source = /home/"${user}"/nixos/hosts/opx5090/home-manager/dotfiles/qtile;
      recursive = true;
      };
 
