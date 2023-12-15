37bef136712ff6c036d84e056a50057b4434deb9
Craig Olson <craigdolson@gmail.com>
2023-12-15 11:49:41 -0600

    changing gtk theme

--- hosts/mainsys/home.nix
+++ hosts/mainsys/home.nix
@@ -18,15 +18,15 @@
   home.homeDirectory = "/home/craig";
   home.stateVersion = "23.05";
   
-gtk = {
+  gtk = {
     enable = true;
     theme = {
       name = "Catppuccin-Macchiato-Compact-Pink-Dark";
       package = pkgs.catppuccin-gtk.override {
-        accents = [ "pink" ];
+        accents = [ "lavender" ];
         size = "compact";
         tweaks = [ "rimless" "black" ];
-        variant = "macchiato";
+        variant = "mocha";
       };
     };
   };
