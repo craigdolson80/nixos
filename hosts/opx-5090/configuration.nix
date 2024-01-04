ec89fabfbd4a6c240c33f888cbac7b3510dd718b
Craig Olson <craigdolson@gmail.com>
2024-01-04 14:06:03 -0600

    updating zsh alias for tailscale

--- hosts/opx-5090/configuration.nix
+++ hosts/opx-5090/configuration.nix
@@ -100,7 +100,7 @@ in
   # Enable CUPS to print documents.
   services.printing.enable = true;
   services.avahi.enable = true;
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
   services.avahi.openFirewall = true;
 
   # Enable sound with pipewire.
