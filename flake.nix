f5f85cd87138626234b7cf1ec97ec7317d73e257
Craig Olson <craigdolson@gmail.com>
2024-01-08 14:29:21 -0600

    updating removing hyprland flake

--- flake.nix
+++ flake.nix
@@ -2,23 +2,20 @@
   description = "NixOS configuration";
 
   inputs = {
-    hyprland.url = "github:hyprwm/Hyprland";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, hyprland, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       mainsys = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          hyprland.nixosModules.default
-          { programs.hyprland.enable = true; }
           hosts/mainsys/configuration.nix
           home-manager.nixosModules.home-manager
           {
-            home-manager.extraSpecialArgs = { inherit inputs; };
+           
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.craig = import hosts/mainsys/home.nix;
