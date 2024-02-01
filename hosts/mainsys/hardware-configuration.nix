b1a7549442e986ec810ea763dfb64ab2784d33ee
Craig Olson <craigdolson@gmail.com>
2024-01-31 22:40:32 -0600

    fixing fucking disk issues

--- hosts/mainsys/hardware-configuration.nix
+++ hosts/mainsys/hardware-configuration.nix
@@ -34,7 +34,6 @@
     fsType = "ext4";
     };
     
-  }  
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
