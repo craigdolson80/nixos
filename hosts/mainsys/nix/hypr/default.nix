3658a2b3a28a192d97c6b73dc326ed2009726b4c
Craig Olson <craigdolson@gmail.com>
2024-02-09 14:32:04 -0600

    adding bitwarden floating window

--- hosts/mainsys/nix/hypr/default.nix
+++ hosts/mainsys/nix/hypr/default.nix
@@ -135,7 +135,7 @@ windowrule = float, ^(waypaper)$
 windowrule = float, ^(Bitwarden)$
 
 # Example windowrule v2
- windowrulev2 = float,class:^(firefox)$,title:(.*)(- Bitwarden)$
+ windowrulev2 = float,class:(firefox)$,title:(.*)(Bitwarden)$
 # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
 
 
