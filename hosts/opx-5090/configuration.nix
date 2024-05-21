3015d12f2de6a762219058b89bcecb46d406339b
Craig Olson <craigdolson@gmail.com>
2024-05-21 09:25:33 -0500

    moving to stable branch

--- hosts/opx-5090/configuration.nix
+++ hosts/opx-5090/configuration.nix
@@ -116,7 +116,7 @@ in
   # Enable CUPS to print documents.
   services.printing.enable = true;
   services.avahi.enable = true;
-  services.avahi.nssmdns4 = true;
+  services.avahi.nssmdns = true;
   services.avahi.openFirewall = true;
 
   # Enable sound with pipewire.
