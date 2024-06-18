2b63c9273313182067e93188cc6d96aeacbad0ec
Craig Olson <craigdolson@gmail.com>
2024-06-18 09:41:19 -0500

    updating flake

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "NixOS configuration";
 
   inputs = {
-    hyprland.url = "github:hyprwm/Hyprland";
+    #hyprland.url = "github:hyprwm/Hyprland";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, hyprland, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       mainsys = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
