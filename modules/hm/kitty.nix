adf4938f7929dd2730335193df8d7e8b9ba52f6e
Craig Olson <craigdolson@gmail.com>
2024-06-26 13:55:41 -0500

    changing kitty theme to dracula

--- modules/hm/kitty.nix
+++ modules/hm/kitty.nix
@@ -8,7 +8,7 @@
   };
 	programs.kitty = {
 	extraConfig = ''
-	include /home/craig/nixos/modules/hm/commonfiles/kitty/kitty-themes/themes/mocha.conf
+	include /home/craig/nixos/modules/hm/commonfiles/kitty/kitty-themes/themes/Dracula.conf
 	confirm_os_window_close 0
         background_opacity 0.8
         dynamic_background_opacity yes
