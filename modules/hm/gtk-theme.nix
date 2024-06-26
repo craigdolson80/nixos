8dce0944767b39553fe154b7edc066c04721ff65
Craig Olson <craigdolson@gmail.com>
2024-06-26 07:25:42 -0500

    changing to dracula theme

--- modules/hm/gtk-theme.nix
+++ modules/hm/gtk-theme.nix
@@ -5,43 +5,43 @@
 {
 
 #  Catppuccin
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Catppuccin-Mocha-Compact-Green-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "green" ];
-        size = "compact";
-        tweaks = [ "black" "rimless" ];
-        variant = "mocha";
-      };
-    };
-  iconTheme = {
-         name = "Papirus-Dark";
-         package = pkgs.catppuccin-papirus-folders.override {
-          accent = "sapphire";
-          flavor = "mocha";
-         };
-    };
-  };
-  home = {
-    pointerCursor = {
-        name = "Catppuccin-Mocha-Dark-Cursors";
-        package = pkgs.catppuccin-cursors.mochaDark;
-        size = 16;
-    };
-  };
+  # gtk = {
+  #   enable = true;
+  #   theme = {
+  #     name = "Catppuccin-Mocha-Compact-Green-Dark";
+  #     package = pkgs.catppuccin-gtk.override {
+  #       accents = [ "green" ];
+  #       size = "compact";
+  #       tweaks = [ "black" "rimless" ];
+  #       variant = "mocha";
+  #     };
+  #   };
+  # iconTheme = {
+  #        name = "Papirus-Dark";
+  #        package = pkgs.catppuccin-papirus-folders.override {
+  #         accent = "sapphire";
+  #         flavor = "mocha";
+  #        };
+  #   };
+  # };
+  # home = {
+  #   pointerCursor = {
+  #       name = "Catppuccin-Mocha-Dark-Cursors";
+  #       package = pkgs.catppuccin-cursors.mochaDark;
+  #       size = 16;
+  #   };
+  # };
 
 #  Dracula
-#  gtk = {
-#    enable = true;
-#    theme = {
-#      name = "Dracula";
-#      package = pkgs.dracula-theme;
-#   };
-#    cursorTheme.name = "Dracula-cursors";
-#    iconTheme.name = "Dracula";
-#    };  
+ gtk = {
+   enable = true;
+   theme = {
+     name = "Dracula";
+     package = pkgs.dracula-theme;
+  };
+   cursorTheme.name = "Dracula-cursors";
+   iconTheme.name = "Dracula";
+   };  
 #randomSHIT
 #trying to fix my gtk themes
 
