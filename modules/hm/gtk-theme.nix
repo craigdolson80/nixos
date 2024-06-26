66fef439db8bb0005ddf54b722ae38550b293cf7
Craig Olson <craigdolson@gmail.com>
2024-06-26 07:28:39 -0500

    changing to dracula theme

--- modules/hm/gtk-theme.nix
+++ modules/hm/gtk-theme.nix
@@ -4,34 +4,6 @@
 
 {
 
-#  Catppuccin
-  # gtk = {
-  #   enable = true;
-  #   theme = {
-  #     name = "Catppuccin-Mocha-Compact-Green-Dark";
-  #     package = pkgs.catppuccin-gtk.override {
-  #       accents = [ "green" ];
-  #       size = "compact";
-  #       tweaks = [ "black" "rimless" ];
-  #       variant = "mocha";
-  #     };
-  #   };
-  # iconTheme = {
-  #        name = "Papirus-Dark";
-  #        package = pkgs.catppuccin-papirus-folders.override {
-  #         accent = "sapphire";
-  #         flavor = "mocha";
-  #        };
-  #   };
-  # };
-  # home = {
-  #   pointerCursor = {
-  #       name = "Catppuccin-Mocha-Dark-Cursors";
-  #       package = pkgs.catppuccin-cursors.mochaDark;
-  #       size = 16;
-  #   };
-  # };
-
 #  Dracula
  gtk = {
    enable = true;
@@ -42,7 +14,5 @@
    cursorTheme.name = "Dracula-cursors";
    iconTheme.name = "Dracula";
    };  
-#randomSHIT
-#trying to fix my gtk themes
 
 }
