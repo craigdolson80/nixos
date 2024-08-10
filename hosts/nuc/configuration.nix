0cb785cbb6ac0d035a7e3d6e07504ad08d041338
Craig Olson <craigdolson@gmail.com>
2024-08-10 16:14:30 -0500

    adding samba

--- hosts/nuc/configuration.nix
+++ hosts/nuc/configuration.nix
@@ -48,16 +48,16 @@ in
 # # Enable SMB Share
    
   # For mount.cifs, required unless domain name resolution is not needed.
-#  environment.systemPackages = [ pkgs.cifs-utils ];
-#  fileSystems."/home/craig/synology" = {
-#    device = "//100.84.38.63/craig";
-#    fsType = "cifs";
-#    options = let
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/home/craig/synology" = {
+    device = "//100.84.38.63/craig";
+    fsType = "cifs";
+    options = let
       # this line prevents hanging on network split
-#      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-#    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
-#  };
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+  };
    
     
   # Set your time zone.
