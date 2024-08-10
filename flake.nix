faf1eddca24eb59464ef45c6b946eac395ceaef5
Craig Olson <craigdolson@gmail.com>
2024-08-10 15:24:21 -0500

    adding new host

--- flake.nix
+++ flake.nix
@@ -46,12 +46,12 @@
       nuc = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          hosts/nuk/configuration.nix
+          hosts/nuc/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.craig = import hosts/nuk/home.nix;
+            home-manager.users.craig = import hosts/nuc/home.nix;
 
           }
         ];
