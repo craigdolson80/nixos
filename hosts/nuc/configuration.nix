fd4ec10087a3a1e91d58511c76a4d12c2bec63ec
Craig Olson <craigdolson@gmail.com>
2024-08-10 14:08:20 -0500

    adding new host

---
+++ hosts/nuc/configuration.nix
@@ -0,0 +1,155 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  user="craig";
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      # Include modules
+      ../../modules/sys/fonts.nix
+      ../../modules/sys/vm.nix
+      ../../modules/sys/polkit.nix
+      ../../modules/sys/syspkgs.nix
+      ../../modules/sys/hypr.nix
+      ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-9ed26e31-72b2-410e-b66b-7f41485b6cc7".device = "/dev/disk/by-uuid/9ed26e31-72b2-410e-b66b-7f41485b6cc7";
+  networking.hostName = "nuc"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+   # Enable Tailscale
+  services.tailscale.enable = true;
+  services.tailscale.useRoutingFeatures = "client";
+  networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
+  networking.search = [ "tail72594.ts.net" ];
+  
+#  fileSystems."/home/craig/nfs" = {
+#    device = "qnap:/craig";
+#    fsType = "nfs";
+#    options = [ "x-systemd.automount" "noauto" ];
+#
+#  };
+
+# # Enable SMB Share
+   
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/home/craig/synology" = {
+    device = "//100.84.38.63/craig";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+  };
+   
+    
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.xserver.windowManager.bspwm.enable = true;
+  services.xserver.windowManager.qtile.enable = true;
+  programs.hyprland.xwayland.enable = true;
+  programs.hyprland.enable = true;
+  programs.sway.enable = true;
+  services.xserver.windowManager.qtile.extraPackages = p: with p; [ qtile-extras ];
+
+  # MISC Services to enable
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+  services.pcscd.enable = true;
+  programs.dconf.enable = true;
+  services.flatpak.enable = true;
+  xdg.portal = {
+     enable = true;
+     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+  # services.onedrive.enable = true;
+  virtualisation.podman.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns4 = true;
+  services.avahi.openFirewall = true;
+
+  # Enable sound with pipewire.
+  #sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  
+  # User Config
+  users.users.craig = {
+    isNormalUser = true;
+    description = "${user}";
+    extraGroups = [ "networkmanager" "wheel" "audio" "video" "lpd" "libvirtd" "users" ];
+    packages = with pkgs; [
+    # firefox
+    ];
+    shell = pkgs.zsh;
+  };
+
+  #Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  #Set shell
+  programs.zsh.enable = true;
+  programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+
+  system.stateVersion = "23.05";
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+### HOME MANAGER MODULE - MOVED TO FLAKE ###
+  
+}
---
+++ hosts/nuc/nix/waybar/waybar.nix
@@ -0,0 +1,415 @@
+{ config, lib, pkgs, ...}:
+
+{
+  programs.waybar = {
+    enable = true;
+    package = pkgs.waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true"] ;} );
+    systemd = {
+      enable = false;
+      target = "graphical-session.target";
+    };
+    
+    settings = {
+      mainBar = {
+        layer = "top";
+        height = 38;
+        modules-left = ["custom/launcher" "hyprland/workspaces" "hyprland/mode" "hyprland/scratchpad" "custom/media"];
+        modules-center = ["hyprland/window"];
+        modules-right = ["custom/cycle_wall" "pulseaudio" "cpu" "memory" "disk" "clock" "tray"];
+      };
+    
+     "hyprland/workspaces" = {
+         "disable-scroll" = "true";
+         "all-outputs" = "true";
+         "warp-on-scroll" = "false";
+         "format" = "{name}";
+         "format-icons" = {
+             "1" = "";
+             "2" = "";
+             "3" = "";
+             "4" = "";
+             "5" = "";
+             "6" = "";
+             "7" = "";
+             "8" = "";
+             "9" = "";
+             "urgent" = "";
+             "focused" = "";
+             "default" = "";
+         };
+     
+    "keyboard-state" = {
+        "numlock" = "true";
+        "capslock" = "true";
+        "format" = "{name} {icon}";
+        "format-icons" = {
+            "locked" = "";
+            "unlocked" = "";
+            };
+        };
+    "hyprland/mode" = {
+        "format" = "<span style=\"italic\">{}</span>";
+    };
+    
+    "hyprland/scratchpad" = {
+        "format" = "{icon} {count}";
+        "show-empty" = "false";
+        "format-icons" = ["" ""];
+        "tooltip" = "true";
+        "tooltip-format" = "{app}: {title}";
+    };
+
+    "tray" = {
+        "spacing" = "10";
+    };
+
+    
+    "clock" = {
+        "format" = "{: %R   %m/%d}";
+        "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+    };
+
+    "cpu" = {
+        "format" = "{usage}% ";
+        "tooltip" = "false";
+        
+    };
+    "memory" = {
+        "format" = "{}% 󰟜";
+    };
+
+    "disk" = {
+        "interval" = "30";
+        "format" = "{percentage_used}% ";
+        "path" = "/";
+    };
+
+    "battery" = {
+        "states" = {
+            "good" = "95";
+            "warning" = "30";
+            "critical" = "15";
+        };
+        "format" = "{capacity}% {icon}";
+        "format-charging" = "{capacity}% ";
+        "format-plugged" = "{capacity}% ";
+        "format-alt" = "{time} {icon}";
+        "format-icons" = ["" "" "" "" ""];
+    };
+    
+    "network" = {
+        "format-wifi" = "{essid} ({signalStrength}%) ";
+        "format-ethernet" = "{ipaddr}/{cidr} ";
+        "tooltip-format" = "{ifname} via {gwaddr} ";
+        "format-linked" = "{ifname} (No IP) ";
+        "format-disconnected" = "Disconnected ⚠";
+        "format-alt" = "{ifname} = {ipaddr}/{cidr}";
+    };
+    
+    "pulseaudio" = {
+        "format" = "{volume}% {icon} {format_source}";
+        "format-bluetooth" = "{volume}% {icon} {format_source}";
+        "format-bluetooth-muted" = " {icon} {format_source}";
+        "format-muted" = " {format_source}";
+        "format-source" = "{volume}% ";
+        "format-source-muted" = "";
+        "format-icons" = {
+            "headphone" = "";
+            "hands-free" = "";
+            "headset" = "";
+            "phone" = "";
+            "portable" = "";
+            "car" = "";
+            "default" = ["" "" ""];
+        };
+        "on-click" = "pavucontrol";
+    };
+
+    "custom/launcher" = {
+        "format" = "";
+        "on-click" = "wlogout";
+   };
+   
+   "custom/cycle_wall" = {
+    "format" = " ";
+    "on-click" = "~/.config/hypr/scripts/changeWallpaper";
+    "tooltip-format" = "Change wallpaper";
+    };
+    
+    "custom/weather" = {
+        "tooltip" = "true";
+        "format" = "{}";
+        "interval" = "30";
+        "exec" = "~/.config/waybar/scripts/waybar-wttr.py";
+        "return-type" = "json";
+    };      
+
+style = ''
+    * {
+    border: none;
+    border-radius: 0;
+    font-family: Iosveka Nerd Font, monospace;
+    font-weight: bold;
+    font-size: 12px;
+    min-height: 0;
+}
+
+window#waybar {
+    background: rgba(25, 25, 25, 0);
+    color: #cdd6f4;
+    padding: 0;
+    margin: 0;
+}
+
+tooltip {
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 10px;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #181825;
+}
+
+#workspaces button {
+    padding: 5px;
+    color: #cdd6f4;
+    background: transparent;
+    margin-right: 5px;
+    border-radius: 5px;
+}
+
+#workspaces button.active {
+    color: #1e1e2e;
+    background: #a6e3a1;
+    border-radius: 5px;
+}
+
+#workspaces button.focused {
+    color: #a6adc8;
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 5px;
+}
+
+#workspaces button.urgent {
+    color: #11111b;
+    background: #f2cdcd;
+    border-radius: 5px;
+}
+
+#workspaces button:hover {
+    background: #89dceb;
+    color: #ffffff;
+    border-radius: 5px;
+}
+
+#custom-language,
+#custom-updates,
+#custom-caffeine,
+#custom-weather,
+#custom-cycle_wall,
+#custom-launcher,
+#window,
+#cpu,
+#memory,
+#disk,
+#clock,
+#battery,
+#pulseaudio,
+#network,
+#workspaces,
+#tray,
+#backlight {
+    background: #1e1e2e;
+    padding: 0px 10px;
+    margin: 3px 0px;
+    margin-top: 10px;
+    border: 1px solid #181825;
+}
+
+#custom-launcher {
+    color: #89dceb;
+    font-size: 15px;
+    border-radius: 5px 0px 0px 5px;
+    background: rgba(25, 25, 25, 0.65);
+    margin-left: 5px;
+    padding-right: 10px;
+    padding-top: 2px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#workspaces {
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 0px 5px 5px 0px;
+    padding-right: 0px;
+    padding-top: 2px;
+    padding-left: 5px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#custom-caffeine {
+    color: #89dceb;
+    border-radius: 5px 0px 0px 5px;
+    border-right: 0px;
+    margin-left: 5px;
+}
+
+#custom-language {
+    color: #f38ba8;
+    border-left: 0px;
+    border-right: 0px;
+}
+
+#custom-updates {
+    color: #f5c2e7;
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 5px 0px 0px 5px;
+    border-left: 0px;
+    border-right: 0px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#window {
+    border-radius: 5px;
+    margin-left: 5px;
+    margin-right: 60px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+    background: rgba(25, 25, 25, 0.65);
+}
+
+#clock {
+    color: @peach;
+    border-radius: 5px 5px 5px 5px;
+    background: rgba(25, 25, 25, 0.65);
+    margin-right: 10px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+    
+}
+
+#network {
+    color: #f9e2af;
+    border-radius: 5px 5px 5px 5px;
+    background: rgba(25, 25, 25, 0.65);
+    margin-right: 10px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#pulseaudio {
+    color: #94e2d5;
+    background: rgba(25, 25, 25, .65);
+    border-radius: 5px 5px 5px 5px;
+    margin-right: 10px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#battery {
+    color: #a6e3a1;
+    border-radius: 0px 5px 5px 0;
+    margin-right: 10px;
+    border-left: 0px;
+}
+
+#custom-weather {
+    border-radius: 5px 5px 5px 5px;
+    background: rgba(25, 25, 25, 0.65);
+    margin-right: 5px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#cpu {
+    color: #cba6f7;
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 5px 0px 0px 5px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#memory {
+    color: #cba6f7;
+    background: rgba(25, 25, 25, 0.65);
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#disk {
+    color: #cba6f7;
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 0px 5px 5px 0px;
+    margin-right: 10px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#custom-cycle_wall {
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 5px 5px 5px 5px;
+    margin-right: 5px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#custom-cycle_wall {
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 5px 5px 5px 5px;
+    margin-right: 5px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#custom-weather {
+    background: rgba(25, 25, 25, 0.65);
+    border-radius: 5px 5px 5px 5px;
+    margin-right: 5px;
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+#tray {
+    border-radius: 5px;
+    margin-right: 5px;
+    background: rgba(25, 25, 25, 0.65);
+    border-top: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-right: 0px solid transparent;
+    border-left: 0px solid transparent;
+}
+
+'';
+};
+};
+};
+}
