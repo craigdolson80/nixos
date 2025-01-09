5c64a9e5706388bfd605706c2e1a2b88d44d3618
Craig Olson <craigdolson@gmail.com>
2025-01-09 14:25:02 -0600

    adding flameshot overlay

--- overlays/flameshot-overlay.nix
+++ overlays/flameshot-overlay.nix
@@ -1,6 +1,6 @@
 self: super: {
   flameshot = super.flameshot.overrideAttrs (oldAttrs: rec {
-    buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.wayland pkgs.grim ]; # Ensure Wayland and grim are available
+    #buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.wayland pkgs.grim ]; # Ensure Wayland and grim are available
     cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [ "-DUSE_WAYLAND_GRIM=ON" ];  # Enable the flag
   });
 }
