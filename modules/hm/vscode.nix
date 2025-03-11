ac4ec7bd4f8be3c0c321ee2827f20392c01e9e0b
Craig Olson <craigdolson@gmail.com>
2025-03-11 07:52:11 -0500

    updating flake

--- modules/hm/vscode.nix
+++ modules/hm/vscode.nix
@@ -3,16 +3,16 @@
 {
  #VSCODE
      programs.vscode.enable = true;
-     programs.vscode.enableExtensionUpdateCheck = true;
-     programs.vscode.enableUpdateCheck = true;
-     programs.vscode.extensions = [
+     programs.vscode.profiles.default.enableExtensionUpdateCheck = true;
+     programs.vscode.profiles.default.enableUpdateCheck = true;
+     programs.vscode.profiles.default.extensions = [
 	pkgs.vscode-extensions.ms-python.python
         pkgs.vscode-extensions.jnoortheen.nix-ide
         pkgs.vscode-extensions.dracula-theme.theme-dracula
         pkgs.vscode-extensions.catppuccin.catppuccin-vsc
         pkgs.vscode-extensions.catppuccin.catppuccin-vsc-icons
          ];
-     programs.vscode.userSettings = {
+     programs.vscode.profiles.default.userSettings = {
         "editor.fontFamily" = "'Droid Sans Mono', 'monospace', monospace, 'FontAwesome'";
         "workbench.colorTheme" = "Catppuccin Mocha";
         "security.workspace.trust.untrustedFiles" = "open";
