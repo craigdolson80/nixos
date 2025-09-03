32d0b3c62e42bd96c6d3a5da645ed8d46a4bf297
Craig Olson <craigdolson@gmail.com>
2025-09-03 07:36:12 -0500

    updating ssh.nix file with new syntax

--- modules/hm/ssh.nix
+++ modules/hm/ssh.nix
@@ -2,8 +2,8 @@
   #SSH
   programs.ssh = {
    enable = true;
-   matchBlocks.*.forwardAgent = true;
-   matchBlocks.*.hashKnownHosts = true;
+   matchBlocks.forwardAgent = true;
+   matchBlocks.hashKnownHosts = true;
    };
 
 }
