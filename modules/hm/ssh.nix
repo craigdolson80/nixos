22b2c3b6dd4f868c61aa6894d96cd1bda65599da
Craig Olson <craigdolson@gmail.com>
2025-09-09 19:57:16 -0500

    updating ssh.nix data per hm changes

--- modules/hm/ssh.nix
+++ modules/hm/ssh.nix
@@ -1,9 +1,29 @@
 {
   #SSH
-  programs.ssh = {
-   enable = true;
-   forwardAgent = true;
-   hashKnownHosts = true;
-   };
+#  programs.ssh = {
+#   enable = true;
+#   forwardAgent = true;
+#   hashKnownHosts = true;
+#   };
+
+programs.ssh = {
+  enable = true;
+
+  # stop relying on default values
+  enableDefaultConfig = false;
+
+  # define defaults for all hosts
+  matchBlocks."*" = {
+    hashKnownHosts = true;
+    forwardAgent = true;
+  };
+
+  # example: custom settings for github.com
+  matchBlocks."github.com" = {
+    user = "git";
+    identityFile = "~/.ssh/id_ed25519";
+  };
+};
+
 
 }
