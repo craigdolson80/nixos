02476e707029778124d4a47de5df6d2b38ce1a00
Craig Olson <craigdolson@gmail.com>
2025-09-15 10:01:31 -0500

    reverting ssh.nix commands back to previous supported commands

--- modules/hm/ssh.nix
+++ modules/hm/ssh.nix
@@ -1,29 +1,29 @@
 {
   #SSH
-#  programs.ssh = {
-#   enable = true;
-#   forwardAgent = true;
-#   hashKnownHosts = true;
-#   };
+  programs.ssh = {
+   enable = true;
+   forwardAgent = true;
+   hashKnownHosts = true;
+   };
 
-programs.ssh = {
-  enable = true;
+#programs.ssh = {
+#  enable = true;
 
-  # stop relying on default values
-  enableDefaultConfig = false;
+#  # stop relying on default values
+#  enableDefaultConfig = false;
 
   # define defaults for all hosts
-  matchBlocks."*" = {
-    hashKnownHosts = true;
-    forwardAgent = true;
-  };
+#  matchBlocks."*" = {
+#    hashKnownHosts = true;
+#    forwardAgent = true;
+#  };
 
   # example: custom settings for github.com
-  matchBlocks."github.com" = {
-    user = "git";
-    identityFile = "~/.ssh/id_ed25519";
-  };
-};
+#  matchBlocks."github.com" = {
+#    user = "git";
+#    identityFile = "~/.ssh/id_ed25519";
+#  };
+#};
 
 
 }
