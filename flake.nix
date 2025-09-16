d891a0b1021fda090ce63695414c1283d68a9f50
Craig Olson <craigdolson@gmail.com>
2025-09-16 11:21:00 -0500

    pinning newer version of 1password in flake

--- flake.nix
+++ flake.nix
@@ -32,13 +32,14 @@
 
             # Pull only 1Password from unstable
             environment.systemPackages = let
-              unstable = import nixpkgs-unstable { system = "x86_64-linux"; };
+              unstable = import nixpkgs-unstable {
+              system = "x86_64-linux";
               config.allowUnfree = true;
+                    };
             in [
-              unstable._1password-gui
-              unstable._1password
-            ];
-          }
+                    unstable._1password-gui
+                    unstable._1password
+            }
 
         ];
       
