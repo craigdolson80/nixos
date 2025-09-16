1d089ddbb4281198f38186b22efdd3c6d7dc45ea
Craig Olson <craigdolson@gmail.com>
2025-09-16 14:12:51 -0500

    trying a 1password overlay

--- overlays/1password-unstable.nix
+++ overlays/1password-unstable.nix
@@ -1,8 +1,7 @@
 final: prev: let
   unstable = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-    # For reproducibility, you can pin to a specific commit:
-    # url = "https://github.com/NixOS/nixpkgs/archive/<commit>.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/6f38b058ccdaa5dccdb058a13a4fffc9477a88dd.tar.gz";
+    sha256 = "05qpki3981zl2jk8pdzhwqidrz99yicbyfv3qb6myigqs9xw2n6n";
   }) {
     inherit (prev) system;
     config.allowUnfree = true;
