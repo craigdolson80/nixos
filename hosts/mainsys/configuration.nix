881f5429ef2cb6d37f9ee0a22eeb79f7fe56243a
Craig Olson <craigdolson@gmail.com>
2025-11-18 18:13:13 -0600

    adding trayscale

--- hosts/mainsys/configuration.nix
+++ hosts/mainsys/configuration.nix
@@ -175,11 +175,11 @@ in
     });
   '';
 
-  {
+  #Temporary
               nixpkgs.config.permittedInsecurePackages = [
                 "python3.12-ecdsa-0.19.1"
               ];
-            }
+            
 
   # List services that you want to enable:
 
