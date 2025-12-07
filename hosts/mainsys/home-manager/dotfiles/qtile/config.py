8510a0604ee9716272e1b282609b38fce0071860
Craig Olson <craigdolson@gmail.com>
2025-12-07 01:37:01 -0600

    updating qtile icons

--- hosts/mainsys/home-manager/dotfiles/qtile/config.py
+++ hosts/mainsys/home-manager/dotfiles/qtile/config.py
@@ -256,7 +256,7 @@ screens = [
                            padding = 6,
                            ),
                 widget.Image(
-                             filename = "~/.config/qtile/icons/python.png",
+                             filename = "~/.config/qtile/icons/nixos_logo_icon_blue.png",
                              mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(mymenu)},
                              scale = "False"
                             ),
