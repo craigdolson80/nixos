90ec8401f3784d5fa632a5d70c15d48a823e7166
Craig Olson <craigdolson@gmail.com>
2026-02-01 22:56:10 -0600

    updating flake with 25-11

--- modules/sys/vm.nix
+++ modules/sys/vm.nix
@@ -1,15 +1,23 @@
 { config, pkgs, ... }:
-{  
+{
   virtualisation = {
     libvirtd = {
       enable = true;
       qemu = {
         swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
+
+        # NOTE: ovmf.* options were removed on recent NixOS.
+        # OVMF firmware is available by default now.
       };
     };
+
     spiceUSBRedirection.enable = true;
   };
+
   services.spice-vdagentd.enable = true;
+
+  # Optional but often needed for UEFI firmware to actually be present on disk:
+  environment.systemPackages = with pkgs; [
+    OVMF
+  ];
 }
