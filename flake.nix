0e1c2b976e0c7b0a248fbe6cb6da3df9453605b9
Craig <craigdolson@gmail.com>
2026-03-26 18:15:32 -0500

    updating flake and hosts

--- flake.nix
+++ flake.nix
@@ -2,18 +2,19 @@
   description = "NixOS configuration";
 
   inputs = {
-    #hyprland.url = "github:hyprwm/Hyprland";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      mainsys = nixpkgs.lib.nixosSystem {
+      t490 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 modules = [
-  ./hosts/mainsys/configuration.nix
+  ./hosts/t490/configuration.nix
   home-manager.nixosModules.home-manager
   {
     # home-manager module settings (system-level)
@@ -24,76 +25,11 @@ modules = [
     home-manager.backupFileExtension = "hm-bak";
 
     # user-level config lives in home.nix
-    home-manager.users.craig = import ./hosts/mainsys/home.nix;
+    home-manager.users.craig = import ./hosts/t490/home.nix;
           }
         ];
       };
-      opx-5090 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          #hyprland.nixosModules.default
-          #{ programs.hyprland.enable = true; }
-          hosts/opx-5090/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            #home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.craig = import hosts/opx-5090/home.nix;
-
-          }
-        ];
-      };
-      nuc = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          hosts/nuc/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.craig = import hosts/nuc/home.nix;
-
-          }
-        ];
-      };
-      virtual = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          hosts/virtual/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.craig = import hosts/virtual/home.nix;
-          }
-        ];
-      };
-      precision-3800 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          hosts/precision-3800/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.craig = import hosts/precision-3800/home.nix;
-          }
-        ];
-      };
-       t480 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          hosts/t480/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.craig = import hosts/t480/home.nix;
-          }
-        ];
-      };
-    };  
-  };
-}  
+    };
+   };
+  }
 
---
+++ hosts/t490/home.nix
@@ -0,0 +1,12 @@
+{config, pkgs, ...}:
+
+{
+
+  home.username = "craig";
+  home.homeDirectory = "/home/craig";
+  home.stateVersion = "26.05";
+
+
+
+}
+
