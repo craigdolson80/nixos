3a976517c388956db08defdd9151b27512174361
Craig <craigdolson@gmail.com>
2026-04-01 23:11:50 -0500

    adding programs in HM

--- hosts/t490/home.nix
+++ hosts/t490/home.nix
@@ -1,6 +1,27 @@
 {config, pkgs, ...}:
-
 {
+ home.packages = with pkgs; [
+     appimage-run
+     alacritty
+     fuzzel
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   home.username = "craig";
   home.homeDirectory = "/home/craig";
