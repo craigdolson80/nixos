56f2f90e2c7aebb31b2de173bf17a9be8f345c9d
Craig Olson <craigdolson@gmail.com>
2026-07-26 07:48:57 -0500

    setting up mainsys

--- hosts/mainsys/configuration.nix
+++ hosts/mainsys/configuration.nix
@@ -17,12 +17,6 @@
   # Firmware and BIOS updates through LVFS/fwupd
   services.fwupd.enable = true;
 
-  networking.hostName = "mainsys"; # Define your hostname.
-  
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Garbage Collection
   nix.gc = {
   automatic = true;
