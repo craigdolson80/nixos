c528bd1305a7ab46da1f6d8d56d79520db74823e
Craig Olson <craigdolson@gmail.com>
2026-07-26 08:08:16 -0500

    setting up mainsys

--- hosts/mainsys/modules/niri/niri-config.nix
+++ hosts/mainsys/modules/niri/niri-config.nix
@@ -63,7 +63,7 @@ in {
     layout = {
       gaps = 12; center-focused-column = "never";
       preset-column-widths = [ { proportion = 0.33333; } { proportion = 0.5; } { proportion = 0.66667; } ];
-      default-column-width.proportion = 0.30;
+      default-column-width.proportion = 0.35;
       focus-ring = { width = 4; active.color = "#94e2d5"; inactive.color = "#505050"; };
       border = { enable = false; width = 4; active.color = "#ffc87f"; inactive.color = "#505050"; urgent.color = "#9b0000"; };
       shadow = { enable = true; softness = 30; spread = 5; offset = { x = 0; y = 5; }; color = "#0007"; };
